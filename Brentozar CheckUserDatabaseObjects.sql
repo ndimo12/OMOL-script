4b91e10b66fb7ae6fb40edc4674e71276510162d
jeslyn12 <100102684+ndimo12@users.noreply.github.com>
2025-12-16 13:03:23 -0600

    Add files via upload

---
+++ Brentozar CheckUserDatabaseObjects.sql
@@ -0,0 +1,17 @@
+
+---"Somebody just handed me a SQL Server. Is it in good shape?" 
+ sp_Blitz 
+
+---"Why is my SQL Server slow right now?"
+sp_BlitzFirst 
+
+--- "What are the most resource-intensive queries on this server?"
+sp_BlitzCache 
+
+--- "How could I tune indexes to make this database faster?"
+sp_BlitzIndex 
+
+---"What queries and tables are involved in deadlocks?"
+sp_BlitzLock 
+
+sp_blitz @CheckServerInfo = 1, @CheckUserDatabaseObjects = 0;  ----With parameters
